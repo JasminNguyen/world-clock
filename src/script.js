3815a38ec8f0adf698327afb9dd9c48ec96ea07c
JasminNguyen <jasminnguyen001@web.de>
2023-06-13 17:07:11 +0200

    Added the JavaScript to isplay the current date and time of a location

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
+//New York
+function updateLosAngelesTime() {
+  let newYorkElement = document.querySelector("#new-york");
+  let newYorkDateElement = newYorkElement.querySelector(".date");
+  let newYorkTimeElement = newYorkElement.querySelector(".time");
+  newYorkDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  newYorkTimeElement.innerHTML = moment()
+    .tz("America/New_York")
+    .format("h:mm:ss [<small>]A<[/small>]");
+}
 
+setInterval(updateLosAngelesTime, 1000);
+
+//Paris
+function updateParisTime() {
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  parisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = moment()
+    .tz("Europe/Paris")
+    .format("h:mm:ss [<small>]A<[/small>]");
+}
+
+setInterval(updateParisTime, 1000);
